897bd1bd63b6155bcdcc541580653ef7d4b8caca
DeveKoke <jorge.be86@gmail.com>
2023-05-07 18:39:19 +0200

    actualizacion

---
+++ index.js
@@ -0,0 +1,16 @@
+// * 1
+let arrayVacio = [];
+
+//  *2 
+let arrayNumeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// * 3
+let arrayNumerosPares = [2, 4, 6, 8];
+
+// * 4
+let arrayBidimensional = [[0, 1, 2], ['a', 'b', 'c']];
+
+//  * 5
+function suma (num1, num2) {
+    
+}
